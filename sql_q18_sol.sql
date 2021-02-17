b20aa4ea59964cba45812e023c742026cc6b3149
vv-me <vvidya25@gmail.com>
2021-02-17 10:16:02 -0800

    Update sql_q18_sol.sql

--- sql_q18_sol.sql
+++ sql_q18_sol.sql
@@ -1,6 +1,6 @@
 USE euro_cup_2016;
 #Write a SQL query to find the highest number of foul cards given in one match.
 SELECT COUNT(*) as HighestFouls from player_booked GROUP BY match_no
-ORDER BY 1 DESC
+ORDER BY HighestFouls DESC
 LIMIT 1
 
