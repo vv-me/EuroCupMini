8f6f78bd473184ba843ae6bea67a80451fee3138
vv-me <vvidya25@gmail.com>
2021-02-17 12:30:39 -0800

    Update sql_q12_sol.sql

--- sql_q12_sol.sql
+++ sql_q12_sol.sql
@@ -1,8 +1,9 @@
 USE euro_cup_2016;
 #Write a SQL query that returns the total number of goals scored by each position on
 #each country’s team. Do not include positions which scored no goals.
-SELECT COUNT(goal_id), pp.position_desc FROM goal_details gd JOIN player_mast pm ON gd.player_id = pm.player_id
+SELECT COUNT(goal_id), sc.country_name, pp.position_desc FROM goal_details gd JOIN player_mast pm ON gd.player_id = pm.player_id
 JOIN playing_position pp ON pm.posi_to_play = pp.position_id
-GROUP BY pp.position_id, pp.position_desc
+JOIN soccer_country sc ON gd.team_id  = sc.country_id 
+GROUP BY pp.position_id, sc.country_name, pp.position_desc
 
 
