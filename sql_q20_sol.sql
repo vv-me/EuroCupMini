60bc132fe0e5b71af2966a243dd470ebe0e64fac
vv-me <vvidya25@gmail.com>
2021-02-17 12:35:59 -0800

    Update sql_q20_sol.sql

--- sql_q20_sol.sql
+++ sql_q20_sol.sql
@@ -2,9 +2,9 @@ USE euro_cup_2016;
 #Write a SQL query to find the substitute players who came into the field in the first
 #half of play, within a normal play schedule.
 -- If you are asking for total count of substitions in all matches
-SELECT COUNT(player_id) FROM player_booked WHERE play_half = 1 and play_schedule = 'NT';
+SELECT COUNT(player_id) FROM player_booked WHERE play_half = 1 and play_schedule = 'NT' and sent_off <> 'Y';
 -- If you are asking for total count of substitions in per match
-SELECT COUNT(player_id), match_no FROM player_booked WHERE play_half = 1 and play_schedule = 'NT'
+SELECT COUNT(player_id), match_no FROM player_booked WHERE play_half = 1 and play_schedule = 'NT' and sent_off <> 'Y'
 GROUP BY match_no;
 -- If you are asking for total count of players who were substituted 
-SELECT COUNT(DISTINCT player_id) FROM player_booked WHERE play_half = 1 and play_schedule = 'NT'
+SELECT COUNT(DISTINCT player_id) FROM player_booked WHERE play_half = 1 and play_schedule = 'NT' and sent_off <> 'Y'
