94e24f1d235cee72e27c9964b985f9996ca36faf
vv-me <vvidya25@gmail.com>
2021-02-17 10:13:30 -0800

    Update sql_q16_sol.sql

--- sql_q16_sol.sql
+++ sql_q16_sol.sql
@@ -4,4 +4,4 @@ USE euro_cup_2016;
 SELECT rm.referee_name, venue_name, COUNT(*) as NumberOfMatches FROM match_mast ma JOIN referee_mast rm 
 ON ma.referee_id = rm.referee_id JOIN soccer_venue sv ON ma.venue_id = sv.venue_id
 GROUP BY ma.referee_id, rm.referee_name, ma.venue_id, venue_name
-ORDER BY 1,2 
+ORDER BY referee_name,venue_name 
