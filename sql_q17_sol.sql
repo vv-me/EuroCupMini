c2903e31af6d726ed6995d7d9326ffd53798cefb
vv-me <vvidya25@gmail.com>
2021-02-17 10:14:15 -0800

    Update sql_q17_sol.sql

--- sql_q17_sol.sql
+++ sql_q17_sol.sql
@@ -1,8 +1,8 @@
 USE euro_cup_2016;
 #Write a SQL query to find the country where the most assistant referees come from,
 #and the count of the assistant referees.
-SELECT sc.country_name as CountryName, COUNT(ass_ref_id)
+SELECT sc.country_name as CountryName, COUNT(ass_ref_id) as Count_assist
 FROM asst_referee_mast arm JOIN soccer_country sc ON arm.country_id = sc.country_id
 GROUP BY arm.country_id, sc.country_name
-ORDER BY 2 DESC
+ORDER BY Count_assist DESC
 LIMIT 1
