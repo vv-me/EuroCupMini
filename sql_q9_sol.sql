2bb278b7fce73da61c1d910f9839a551e4362172
vv-me <vvidya25@gmail.com>
2021-02-17 10:04:56 -0800

    Update sql_q9_sol.sql

--- sql_q9_sol.sql
+++ sql_q9_sol.sql
@@ -3,4 +3,4 @@ USE euro_cup_2016;
 #Germany, who played in Germany’s group stage matches.
 SELECT distinct pm.player_name, pm.jersey_no FROM match_details md JOIN player_mast pm ON md.player_gk = pm.player_id
 AND md.play_stage = "G"
-JOIN soccer_country sc ON pm.team_id = sc.country_id AND sc.country_name = "Germany"
+JOIN soccer_country sc ON pm.team_id = sc.country_id WHERE sc.country_name = "Germany"
