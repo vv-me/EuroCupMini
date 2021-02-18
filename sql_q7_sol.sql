6209b680407569d750aaff219e958df008d43515
vv-me <vvidya25@gmail.com>
2021-02-17 22:21:34 -0800

    Update sql_q7_sol.sql

--- sql_q7_sol.sql
+++ sql_q7_sol.sql
@@ -2,4 +2,4 @@ USE euro_cup_2016;
 #Write a SQL query to find all the venues where matches with penalty shootouts were
 #played.
 SELECT v.venue_name FROM match_mast m JOIN soccer_venue v ON m.venue_id = v.venue_id
-WHERE match_no IN (SELECT match_no FROM penalty_shootout p)
+WHERE match_no IN (SELECT DISTINCT match_no FROM penalty_shootout p)
